d0d49b6449d9d111ca5bba2b17db847b8518d31b
Mikael Brassman <mishakun@gmail.com>
2018-01-14 14:39:34 +0100

    game over sound

--- src/states/GameOver.js
+++ src/states/GameOver.js
@@ -1,5 +1,7 @@
 import throttle from 'lodash.throttle';
 
+import fx from 'wafxr';
+
 /**
  * Setup and display the main game state.
  */
@@ -15,6 +17,8 @@ export default class GameOver extends Phaser.State {
     var surviveText = game.add.text(game.world.centerX, game.world.centerY + 72 - 48,
        'You survived ' + Math.round(Number(game.timer)) + ' seconds of the m.a.r.s zombie apocalypse.\n\nTap anywhere to restart!', {font: '24px VT323', fill: '#F2F2F2', align: 'center'});
     surviveText.anchor.setTo(0.5, 0.2);
+
+    fx.play({"volume":-10,"attack":0.194,"decay":0.305,"sustain":0.2,"release":0.658,"sustainLevel":0.9,"frequency":301,"sweep":-0.44,"source":"square","compressorThreshold":-34.15});
 
     //the "click to restart" handler
   game.input.onTap.addOnce(function () {
